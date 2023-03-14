111681ed3b9bafd9347c2f668340091fe3554dd6
vesanmo1 <veronicasanchez.design@gmail.com>
2023-03-14 12:33:00 +0100

    add id to buttons and more js code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 window.addEventListener("load", function () {
   const nav = document.querySelector("#nav__items");
-  const open = document.getElementById("menu-button");
-  var opened = false;
+  const openMenu = document.getElementById("menu-button");
+  const openSublist = document.getElementById("sublist-button");
+  var openedMenu = false;
+  var openedSublist = false;
 
-  open.addEventListener("click", () => {
-    if (!opened) {
+  openMenu.addEventListener("click", () => {
+    if (!openedMenu) {
       nav.classList.add("visible");
-      opened = true;
+      openedMenu = true;
     } else {
       nav.classList.remove("visible");
-      opened = false;
+      openedMenu = false;
+    }
+  });
+
+  openSublist.addEventListener("click", () => {
+    if (!openedSublist) {
+      nav.classList.add("visible");
+      openedSublist = true;
+    } else {
+      nav.classList.remove("visible");
+      openedSublist = false;
     }
   });
 });
